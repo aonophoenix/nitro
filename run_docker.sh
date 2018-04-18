ccc3cfe42700278dda809f0f584092dcdeb1173d
justin <justin@jshattmac.local>
2018-04-18 17:07:10 -0600

    update shell file and draft
    Add directory check for data and results to shell file. Updated methods in draft.

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,9 @@
+if [ ! -d "data" ]; then
+	mkdir data
+fi
+if [ ! -d "results" ]; then
+	mkdir results
+fi
 docker build -t nitro .
 docker run -ti --name=nitro nitro
 docker cp nitro:/usr/src/nitro/results/allrows.tsv ./results/allrows.tsv
